2b6c036680ee2ac1e2757ffee38865e67fe17e4e
Nickpodski <Nickpodski@gmail.com>
2021-03-27 21:50:04 -0400

    added js code for commenting

---
+++ public/js/comment.js
@@ -0,0 +1,30 @@
+
+const addComment = () => {
+  let newComment = {
+      comment: $('#newComment').val()
+  }
+
+  let id = $('.card').attr('data-id')
+
+  axios.post(`/api/comments/${id}`, newComment)
+      .then(res => {
+          window.location.href = `/post/${id}`;
+      })
+      .catch(err => {
+          console.log(err);
+
+      })
+}
+
+$(document).ready(() => {
+
+  $('#commentBtn').click(() => {
+      $('.commentForm').removeClass('hidden');
+  })
+
+  $('#newCommentForm').submit(e => {
+      e.preventDefault();
+      if ($('#newComment').val() === '') return;
+      addComment();
+  })
+})
